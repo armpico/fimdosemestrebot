4f394865369412f6278fb8987692cbbf4e548210
aBARICHELLO <arturbarichello@hotmail.com>
2018-04-27 12:51:29 -0300

    Refactor

--- bot/core.py
+++ bot/core.py
@@ -6,20 +6,19 @@ from datetime import datetime, date
 from telegram import InlineQueryResultArticle, InputTextMessageContent, ParseMode
 
 tz = pytz.timezone('America/Sao_Paulo')
-TODAY = datetime.now(tz=tz)
 
 def start(bot, update):
-    update.message.reply_text(check(get_end_date() - TODAY))
+    update.message.reply_text(check(get_end_date() - today()))
 
 # Read file and return datetime object
 def get_end_date():
     file = open('file.date', 'r')
-    end_date = datetime.strptime(file.readline(), '%d-%m-%Y %H:%M') # Update end_date
+    end_date = datetime.strptime(file.readline(), '%d-%m-%Y %H:%M')
     return tz.localize(end_date)
 
 # Function called when bot is used inline
 def inline(bot, update):
-    phrase = check(get_end_date() - TODAY)
+    phrase = check(get_end_date() - today())
 
     results = []
     results.append(
@@ -33,22 +32,25 @@ def inline(bot, update):
 
 # Returns a string according to n of days left
 def check(days_left):
-    adaptive_string = strings.START_STRINGS
+    string = strings.START_STRINGS
     if (get_end_date().month >= 6 and get_end_date().month < 8) or get_end_date().month >= 11:
-        adaptive_string = strings.END_STRINGS
+        string = strings.END_STRINGS
 
     if days_left.days < 0:
-        ret = f'{strings.PALM_TREE} O semestre de {get_end_date().year} da UFSC {adaptive_string[0]}! {strings.CONFETTI} {strings.HORN}'
+        ret = f'{strings.PALM_TREE} O semestre de {get_end_date().year} da UFSC {string[0]}! {strings.CONFETTI} {strings.HORN}'
     elif days_left.days == 0:
         hours_left = int(days_left.total_seconds() // 3600)
-        ret = f'{strings.PALM_TREE} O semestre de {get_end_date().year} da UFSC {adaptive_string[1]} em {hours_left} horas'
+        ret = f'{strings.PALM_TREE} O semestre de {get_end_date().year} da UFSC {string[1]} em {hours_left} horas'
     else:
-        ret = f'{strings.PALM_TREE} O semestre de {get_end_date().year} da UFSC {adaptive_string[1]} em {days_left.days} dias'
+        ret = f'{strings.PALM_TREE} O semestre de {get_end_date().year} da UFSC {string[1]} em {days_left.days} dias'
     return ret
+
+def today():
+    return datetime.now(tz=tz)
 
 # Used to change the target date
 def set_date(bot, update, job_queue, chat_data):
-    if update.message.chat.id == config.MAINTAINER: # replace with environment
+    if update.message.chat.id == config.MAINTAINER:
         contents = update.message.text.strip('/set ')
         update.message.reply_text("Set date as: " + contents)
         file = open('file.date', 'w')
